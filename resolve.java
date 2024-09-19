93cd9bbbb565dda05040b18b9ed8cf681e4f5e97
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-09-19 15:45:30 +0530

    This is final project

---
+++ resolve.java
@@ -0,0 +1,9 @@
+public class resolve {
+   public static void main(String[] args) {
+    System.out.println(8+9);
+    int a=9;
+    int b = 7;
+    int sum =a+b;
+    System.out.println(sum);
+   } 
+}
